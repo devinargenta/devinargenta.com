ea15504f4312d47cb066c893d57bcba7c7ddeec5
devinargenta <devin@devinargenta.com>
2020-09-25 14:16:01 -0400

    tracking, ok

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import '../styles/globals.css'
+import * as gtag from '../services/tracking';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
-export default MyApp
+  return <Component {...pageProps} />;
+};
+
+export default App;

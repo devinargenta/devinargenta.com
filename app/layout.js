a90982a3507f7fa5fb22b565f70e6364317c08c4
Devin Argenta <devin@devinargenta.com>
2023-03-03 14:17:47 -0500

    title

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,20 @@
 import "../styles/globals.css";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
-import Nav from "../components/Nav";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>devin argenta</title>
+        <meta
+          name="Description"
+          content="the website of the worlds greatest software engineer (devin argenta) was not able to be found at this location, but this is the website of another one  who lives in brooklyn in new york city "
+        ></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body>
         <div className={`${styles.page} ${styles.dark}`}>
-            <Header />
+          <Header />
           <div className={styles.container}>{children}</div>
         </div>
       </body>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,22 +1,12 @@
 import Section from "./Section";
 import styles from "./Header.module.css";
-import Head from "next/head";
+
 import Nav from "./Nav";
-const Meta = () => (
-  <Head>
-    <title>devin argenta</title>
-    <meta
-      name="Description"
-      content="the website of the worlds greatest software engineer (devin argenta) was not able to be found at this location, but this is the website of another one  who lives in brooklyn in new york city "
-    ></meta>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-  </Head>
-);
+
 export default function Header({ children }) {
   return (
     <Section className={styles.header}>
       <div className={styles.headingWrapper}>
-        <Meta />
         <h1 className={styles.h1}>devin argenta</h1>
         <Nav />
         {children}
